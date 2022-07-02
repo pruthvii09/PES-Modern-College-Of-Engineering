20b6fb13628202e30ed8dc227967354667d3ad1a
Pruthviraj Auti <ytvancedpruthvi@gmail.com>
2022-07-02 18:17:26 +0530

    Admission Section Complete

---
+++ Sub_Files/JS/index.js
@@ -0,0 +1,9 @@
+// console appliocation programming interferece (API)
+
+console.log('Hello World');
+// alert("He He He");
+console.warn("ye warning hain bhaii");
+console.error("ye error hai bhaii");
+
+
+var arr = ["pruthvi", 2, 5, "Pratyush"]
